636e6ba4e17d7475ccd0736e5816e4e56566f3a4
ben-eliav <eliav123.spam@gmail.com>
2024-05-26 18:11:20 +0300

    change sweep parameters

--- final_sweep.py
+++ final_sweep.py
@@ -19,15 +19,11 @@ sweep_config = {
     },
     "parameters": {
         "ENV_HPT_mode": {"values": [False]},
-        "architecture": {"values": ["transformer"]},
-        "seed": {"values": list(range(1, 6))},
-        "save_previous_games": {"values": [True]},
+        "architecture": {"values": ["lstm"]},
         "combine_features": {"values": [True]},
         "seed": {"values": [1, 2, 3]},
         "feature_combination": {"values": [["EFs", "GPT4"], ["EFs", "BERT"]]},
-        "transformer_nheads": {"values": [2, 4, 8]},
         "hidden_dim": {"values": [32, 64, 128]},
-        "layers": {"values": [1, 2]},
         "ENV_LEARNING_RATE": {"values": [1e-3, 1e-4, 1e-5]},
     },
     "command": command
