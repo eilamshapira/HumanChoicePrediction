43f5ee1d4b8d6918a73dd6534adf52a1f470294d
eden-hindi <edenhindi1@gmail.com>
2024-06-08 23:28:44 +0300

    fixing the gradient update and changing values for the sweep

--- RunningScripts/clone_and_init_edenhindi.sh
+++ RunningScripts/clone_and_init_edenhindi.sh
@@ -1,8 +1,8 @@
 # cloning your code from github:
-git clone https://github.com/eilamshapira/NLP_project.git
+git clone https://github.com/edenhindi/HumanChoicePrediction.git
 
 # Your main sweep:
-python final_sweep_YOUR_UID.py
+python final_sweep_edenhindi.py
 
 # More runs appear in your report:
 # python sweep_1.py
