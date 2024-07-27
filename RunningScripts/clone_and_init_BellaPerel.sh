be0a59d5b62558016adc63ecc74d3753dc43c908
BellaPerel <81310709+BellaPerel@users.noreply.github.com>
2024-07-27 18:21:07 +0300

    Analysis notebook

---
+++ RunningScripts/clone_and_init_BellaPerel.sh
@@ -0,0 +1,10 @@
+# cloning your code from github:
+git clone https://github.com/BellaPerel/HumanChoicePrediction.git
+
+# Your main sweep:
+python final_sweep_BellaPerel.py
+
+# More runs appear in your report:
+# python sweep_1.py
+# python sweep_2.py
+# python sweep_3.py
